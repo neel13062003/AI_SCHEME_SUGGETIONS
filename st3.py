9bdff1026fa20e5007d9face33086b25183d5c58
darshanegniol <darshansatapara@egniol.co.in>
2025-06-17 10:42:29 +0000

    changfe the name

--- st3.py
+++ st3.py
@@ -28,7 +28,7 @@ st.set_page_config(page_title="Scheme Matcher", layout="wide")
 
 try:
     client = OpenAI(
-        api_key=st.secrets["api_keys"]["openai"],
+        api_key=st.secrets["database"]["openai"],
         proxies=None
     )
 except (KeyError, TypeError, ValueError) as e:
